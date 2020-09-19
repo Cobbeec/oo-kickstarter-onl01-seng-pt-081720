62afcc9c02d03124fa7ed42764d2766db751efd8
Elizabeth Cobb <cobbeec@gmail.com>
2020-09-19 00:45:26 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -5,8 +5,8 @@ class Backer
     @name = name
     @backed_projects = []
   end
+  
    def back_project(project)
     @backed_projects << project
-    project.add_backer(self) unless project.backers.include?(self)
   end
 end 
